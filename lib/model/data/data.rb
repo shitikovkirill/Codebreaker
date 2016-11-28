5c77442f258edd3225774f80d16a850eb95bef8e
shitikovkirill <sh.kiruh@gmail.com>
2016-11-28 09:20:18 +0200

    add

--- lib/model/data/data.rb
+++ lib/model/data/data.rb
@@ -3,19 +3,16 @@ module Codebreaker
     class File
       class YAML
 
-        def initialize (file_name = 'data_file_yaml.yaml')
+        def initialize(file_name = 'data_file_yaml.yaml')
           @file_name = file_name
         end
 
         def load_data
-          if (::File.exist?(@file_name))
-            ::YAML.load_file(@file_name)
-          else
-            nil
-          end
+          return unless ::File.exist?(@file_name)
+          ::File.exist?(@file_name)
         end
 
-        def save_data (object)
+        def save_data(object)
           ::File.open(@file_name, "w+") do |file|
             file.puts object.to_yaml
           end
